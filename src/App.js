c1eba6752540c477d42bdb8b07cb0a04bc1f95b4
Sergio Campbell Dev <51247757+SergioCampbell@users.noreply.github.com>
2022-11-23 21:15:22 -0500

    First Commit: adding project to repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Col } from 'antd';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import './App.css';
+import logo from './statics/logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pekedux :v'/>
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
